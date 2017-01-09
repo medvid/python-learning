d2fb0ab14d178c938af1e864eec90e12e1957328
Volodymyr Medvid <vmedvid@riseup.net>
2017-01-09 20:22:15 +0200

    Add text analyzer and test files

---
+++ lesson03/test_text_analyzer.py
@@ -0,0 +1,55 @@
+import unittest
+from text_analyzer import *
+
+class TestTextAnalyzer(unittest.TestCase):
+    def test_is_word(self):
+        self.assertTrue(is_word("latin"))
+        self.assertTrue(is_word("кирилиця"))
+        self.assertTrue(is_word("汉语; 漢語"))
+        self.assertTrue(is_word("日本語"))
+        self.assertTrue(is_word("العَرَبِيَّة"))
+
+    def test_is_not_word(self):
+        self.assertFalse(is_word(""))
+        self.assertFalse(is_word(" "))
+        self.assertFalse(is_word(".!@#$%^&*()"))
+        self.assertFalse(is_word("  .  "))
+        self.assertFalse(is_word("?!"))
+
+    def test_is_char(self):
+        self.assertTrue(is_char("a"))
+        self.assertTrue(is_char("Є"))
+        self.assertTrue(is_char("語"))
+
+    def test_is_not_char(self):
+        self.assertFalse(is_char(""))
+        self.assertFalse(is_char(" "))
+        self.assertFalse(is_char("."))
+        self.assertFalse(is_char("/e"))
+
+    def test_count_words(self):
+        self.assertEqual(count_words(""), 0)
+        self.assertEqual(count_words("a"), 1)
+        self.assertEqual(count_words("?   "), 0)
+        self.assertEqual(count_words("x ^ y"), 2)
+        self.assertEqual(count_words("x^y"), 1)
+        self.assertEqual(count_words("x^   y"), 2)
+        self.assertEqual(count_words("^^^ ^^"), 0)
+        self.assertEqual(count_words("ddddd   dddddd  dddddddddddd"), 3)
+
+    def test_count_chars(self):
+        self.assertEqual(count_chars(""), 0)
+        self.assertEqual(count_chars("A"), 1)
+        self.assertEqual(count_chars("тест"), 4)
+        self.assertEqual(count_chars("!zz zz!"), 7)
+
+    def test_process_unique_words(self):
+        words = set()
+        self.assertEqual(len(words), 0)
+        process_unique_words("aa bb cc", words)
+        self.assertEqual(len(words), 3)
+        process_unique_words("cc dd", words)
+        self.assertEqual(len(words), 4)
+
+if __name__ == "__main__":
+    unittest.main();
---
+++ lesson03/text_analyzer.py
@@ -0,0 +1,102 @@
+from os.path import basename, getsize
+from itertools import islice
+from time import time
+import re
+
+# regular expression to detect word by presence of any unicode character
+word_regex = re.compile(r'[\w\d]', re.UNICODE)
+
+# regular expression to detect unicode character
+char_regex = re.compile(r'\w', re.UNICODE)
+
+# regular expression to detect digit or _
+digit_regex = re.compile(r'[0-9_]', re.UNICODE)
+
+
+def is_word(token):
+    return word_regex.search(token)
+
+def is_char(token):
+    # TODO use single regular expression
+    return char_regex.search(token) and not digit_regex.search(token)
+
+def count_words(line):
+    word_count = 0
+    for word in line.split():
+        if (is_word(word)):
+            word_count+=1
+    return word_count
+
+def count_chars(line):
+    char_count = 0
+    for char in line:
+        if (is_char(char)):
+            char_count+=1
+    return char_count
+
+def process_unique_words(line, word_set, word_dict):
+    for word in line.split():
+        if (is_word(word)):
+            word_set.add(word)
+            if word in word_dict:
+                word_dict[word]+=1
+            else:
+                word_dict[word] = 0
+
+def process_unique_chars(line, char_set, char_dict):
+    for char in line:
+        if (is_char(char)):
+            char_set.add(char.upper())
+            if char in char_dict:
+                char_dict[char]+=1
+            else:
+                char_dict[char] = 0
+
+def print_top_words(word_dict, count):
+    for word in islice(sorted(word_dict, key=word_dict.get, reverse=True), count):
+        print("{}: {}".format(word, word_dict[word]))
+
+# any object enumerable as string collection can be passed as filestream
+def analyze_stream(filestream):
+    word_count = 0
+    char_count = 0
+    word_set = set()
+    word_dict = {}
+    char_set = set()
+    char_dict = {}
+
+    for line in filestream:
+        word_count += count_words(line)
+        char_count += count_chars(line)
+        process_unique_words(line, word_set, word_dict)
+        process_unique_chars(line, char_set, char_dict)
+
+    print("word count: {0}".format(word_count))
+    print("character count: {0}".format(char_count))
+    print("unique word count: {0}".format(len(word_set)))
+    print("unique character count: {0}".format(len(char_set)))
+    # print(sorted(char_set))
+    print("10 most used words:")
+    print_top_words(word_dict, 10)
+    print("10 most used characters:")
+    print_top_words(char_dict, 10)
+
+# open the file using context manager and analyze its contents
+def analyze_file(filename):
+    with open(filename) as filestream:
+        analyze_stream(filestream)
+
+def benchmark_file(filename):
+    print("analyzing {0} ({1:.1f}M)".format(
+        basename(filename),
+        getsize(filename) / 1024 / 1024))
+    t0 = time()
+    analyze_file(filename)
+    t1 = time()
+    print("Time taken: {0:.2f}s".format(t1 - t0))
+    print()
+
+if __name__ == "__main__":
+    benchmark_file("../test_files/bible.txt")
+    benchmark_file("../test_files/quran-simple.txt")
+    # benchmark_file("../test_files/test.txt")
